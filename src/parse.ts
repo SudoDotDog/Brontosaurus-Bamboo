878d9fc4b4781db39de1436ca3724eac5251443f
PCXPY <pc@wmpcxpy.com>
2020-03-19 15:10:56 -0500

    [feat] add parse functions

--- src/parse.ts
+++ src/parse.ts
@@ -26,3 +26,39 @@ export const joinURLFriendlyCombined = (username: string, namespace: string): st
 
     return joinCombinedBySeparator(username, namespace, '_');
 };
+
+export type ParseCombinedResult = {
+
+    readonly method: 'regular' | 'url-friendly' | 'default';
+    readonly username: string;
+    readonly namespace: string;
+};
+
+export const parseUsernameNamespaceCombined = (combined: string): ParseCombinedResult => {
+
+    const regularSplited: string[] = combined.split('/');
+    if (regularSplited.length === 2) {
+
+        return {
+            method: 'regular',
+            username: regularSplited[1],
+            namespace: regularSplited[0],
+        };
+    }
+
+    const urlFriendlySplited: string[] = combined.split('_');
+    if (urlFriendlySplited.length === 2) {
+
+        return {
+            method: 'url-friendly',
+            username: urlFriendlySplited[1],
+            namespace: urlFriendlySplited[0],
+        };
+    }
+
+    return {
+        method: 'default',
+        username: combined,
+        namespace: DEFAULT_BRONTOSAURUS_NAMESPACE.DEFAULT,
+    };
+};
