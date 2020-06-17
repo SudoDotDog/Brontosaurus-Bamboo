af898550dd2291cd22fdb941f1bc8b9efcf1e3f4
WMXPY <wm@mengw.io>
2020-06-17 09:30:27 -0500

    add detail example

---
+++ example/account/detail.ts
@@ -0,0 +1,24 @@
+/**
+ * @author WMXPY
+ * @namespace Bamboo_Account
+ * @description Detail
+ * @override Example
+ */
+
+import { Bamboo } from "../../src/bamboo";
+import { CommonAccountDetailResponse } from "../../src/declare/common";
+
+(async () => {
+
+    const key: string = process.env.KEY as string;
+    const bamboo: Bamboo = Bamboo.create('http://localhost:8500', key);
+
+    const result: CommonAccountDetailResponse = await bamboo.detailAccount({
+
+        type: 'username-namespace',
+
+        username: 'admin',
+        namespace: 'brontosaurus.admin',
+    });
+    console.log(JSON.stringify(result, null, 2));
+})();
